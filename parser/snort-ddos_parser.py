f603c7335cd85c06b7943eb7aabe1b64c96d3b7c
Arnav Mohan <71927128+amohanonamission@users.noreply.github.com>
2026-08-24 21:02:03 -0400

    Update snort-ddos_parser.py

--- parser/snort-ddos_parser.py
+++ parser/snort-ddos_parser.py
@@ -16,15 +16,33 @@ integer = pyp.Word(pyp.nums)
 ip_addr = pyp.Combine(integer+'.'+integer+'.'+integer+'.'+integer+':'+integer)
 
 def snort_parse(logfile):
-  header = (pyp.Suppress("[**] [") + pyp.Combine())
+  header = (pyp.Suppress("[**] [") + pyp.Combine(integer + ":" + integer + ":" + integer) + pyp.Suppress(pyp.SkipTo("[**]", include = True)))
+    pri = pyp.Suppress("[Priority:") + integer + pyp.Suppress("]")
+    date = pyp.Combine(integer+"/"+integer+'-'+integer+':'+integer+':'+integer+'.'+integer)
+    src_ip = ip_addr + pyp.Suppress("->")
+    dest_ip = ip_addr
 
+    bnf = header+pri+date+src_ip+dest_ip
 
-
-
-
-
-
-
-
-
+    with open(logfile) as snort_logfile:
+        for has_content, grp in itertools.groupby(
+                snort_logfile, key = lambda x: bool(x.strip())):
+            if has_content:
+                tmpStr = ''.join(grp)
+                fields = bnf.searchString(tmpStr)
+                try:
+                    if fields[0][2] >= tStamp1:
+                        if fields[0][4] == tomcat:
+#                            print("tomcat-attacked")
+                            subprocess.run('ssh k8s-admin@10.0.0.131 "kubectl scale deployment tomcat -n kube-tomcat --replicas=1"', shell=True, check=True, text=True)
+                        elif fields[0][4] == mongodb:
+#                            print("mongodb-attacked")
+                            subprocess.run('ssh k8s-admin@10.0.0.131 "kubectl scale deployment mongodb -n kube-mongodb --replicas=1"', shell=True, check=True, text=True)
+                        elif fields[0][4] == wordpress:
+#                            print("wordpress-attacked")
+                            subprocess.run('ssh k8s-admin@10.0.0.131 "kubectl scale deployment wordpress --replicas=1"', shell=True, check=True, text=True)
+                except KeyError:
+                    exit
+                except IndexError:
+                    exit
 snort_parse('/var/log/snort/alert')
